8bc1e01495036711a4024dac4800afbb2687132f
AUSpade <125840562+AUSpade@users.noreply.github.com>
2026-03-05 10:39:01 +1100

    Update permissions.js

--- permissions.js
+++ permissions.js
@@ -1,1 +1,46 @@
+/* ---------------- POWER LEVELS ---------------- */
 
+const RANKS = {
+
+"Junior Member":0,
+"Probationary Member":1,
+"Support Member":2,
+"Member Operational":3,
+
+"Section Leader":4,
+"Deputy Controller":5,
+"Controller":6,
+
+"Staff":7,
+
+"Super Admin":100
+
+};
+
+
+/* ---------------- PERMISSION RULES ---------------- */
+
+const PERMISSIONS = {
+
+UMT:4,
+
+VEHICLE_CONTROL:4,
+
+ANNOUNCEMENTS:5,
+
+USER_MANAGEMENT:5,
+
+SUPER_ADMIN:100
+
+};
+
+
+/* ---------------- PERMISSION CHECK ---------------- */
+
+function hasPermission(permission){
+
+const user = users[currentUser];
+
+return user.power >= PERMISSIONS[permission];
+
+}
